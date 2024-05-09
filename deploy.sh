a02a6f2c1359befbc49a761523a23f35e85401e4
RobertLeone <robert.leone25@gmail.com>
2024-05-08 23:44:31 -0300

    Fix script user data

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 sudo yum update -y  
-cd ~/
-aws s3 sync s3://vitibrasil-integrations/project/ . 
-unzip vitibrasil_api.zip
+aws s3 sync s3://vitibrasil-integrations/project/ ~/
+unzip ~/vitibrasil_api.zip -d ~/
