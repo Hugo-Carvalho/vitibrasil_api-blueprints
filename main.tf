eee51a67b4185626579624296644b4e964c345be
RobertLeone <robert.leone25@gmail.com>
2024-05-08 23:10:27 -0300

    Fix tokens

--- main.tf
+++ main.tf
@@ -1,8 +1,15 @@
 provider "aws" {
     region = "us-east-1"
 }
+
+module "up_project_file" {
+  source    = "./modules/S3"
+  file_name = "vitibrasil_api"
+  project_bucket = "vitibrasil-integrations"
+}
 module "up_ec2" {
   source    = "./modules/EC2"
   vpc_id    = "vpc-0a161f64fd9c46cf2"
   subnet_id = "subnet-030b785da2ffa155b"
+  depends_on = [ module.up_project_file ]
 }
---
+++ modules/S3/vars.tf
@@ -0,0 +1,9 @@
+variable "file_name" {
+  type = string
+  description = "Nome do arquivo"
+}
+
+variable "project_bucket" {
+  type = string
+  description = "Nome do bucket"
+}
