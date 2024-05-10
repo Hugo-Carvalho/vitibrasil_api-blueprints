0f8ad189f0b328914235be66aa1107f7dddf1728
Bruno Melo <brunomelo1315@gmail.com>
2024-05-09 21:40:12 -0300

    :rocket: prepare for deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo yum update -y 
 sudo yum install make -y
+sudo amazon-linux-extras install python3.8 -y
 aws s3 sync s3://vitibrasil-integrations/project/ /home/ec2-user/
 unzip /home/ec2-user/vitibrasil_api.zip -d /home/ec2-user/
 cd /home/ec2-user/
