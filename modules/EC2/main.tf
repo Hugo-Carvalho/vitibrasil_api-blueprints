0b0bcc9eedbf1034c8e7f475c77b3ae044fa4ea2
RobertLeone <robert.leone25@gmail.com>
2024-05-09 23:42:27 -0300

    Fix aws cli

--- modules/EC2/main.tf
+++ modules/EC2/main.tf
@@ -134,7 +134,6 @@ resource "aws_security_group" "permitir_ssh_http" {
 
 resource "null_resource" "reboot_instance" {
   provisioner "local-exec" {
-    interpreter = ["/bin/bash", "-c"]
     command     = "aws ec2 reboot-instances --instance-ids ${aws_instance.vitibrasil_instance.id}"
   }
 
