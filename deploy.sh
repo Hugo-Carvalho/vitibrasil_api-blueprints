2319d8a55306895c4e27d2cd412fc89c8891b5fe
RobertLeone <robert.leone25@gmail.com>
2024-05-15 21:12:17 -0300

    Add exclude files

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,7 @@ Content-Disposition: attachment; filename="userdata.txt"
 sudo yum update -y 
 sudo yum install make -y
 sudo amazon-linux-extras install python3.8 -y
+sudo rm -rf /home/ec2-user/
 aws s3 sync s3://vitibrasil-integrations/project/ /home/ec2-user/
 unzip /home/ec2-user/vitibrasil_api.zip -d /home/ec2-user/
 cd /home/ec2-user/
